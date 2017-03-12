8b32f4415aaa856b8ce600bf568364e7ef28e27e
gokhanoguz <m.gokhanoguz@gmail.com>
2017-03-12 15:58:20 +0100

    Create User model

---
+++ app/src/main/java/com/javatar/androidfirabasedatabase/model/User.java
@@ -0,0 +1,18 @@
+package com.javatar.androidfirabasedatabase.model;
+
+/**
+ * Created by gokhan on 12-Mar-17.
+ */
+
+public class User {
+    public String name;
+    public String email;
+
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
